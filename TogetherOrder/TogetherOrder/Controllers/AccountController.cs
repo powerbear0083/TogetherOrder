1273a468681c359b35f8573fbd6f7357cd7b4d44
DEV\Yinhsun.Chou <yinhsun.chou@ehsn.com.tw>
2021-03-03 14:49:37 +0800

    AccountController 新增 SignUp method

---
+++ TogetherOrder/TogetherOrder/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TogetherOrder.Models;
+
+namespace TogetherOrder.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpPost]
+        public async Task<ActionResult<Account>> SignUp(string name, string password)
+        {
+
+
+
+        }
+    }
+    
+}
